0b3d957ee6d5625608ffb89faff376c8b8a09cf5
AndyGrld <ansongandy04@gmail.com>
2022-12-09 17:19:52 -0800

    done

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,16 +3,21 @@
 #include <stdlib.h>
 
 /*betty style*/
+/**
+ * comment for main
+ * check if n is even or odd
+ * return 0
+ */
 int main(void)
 {
 	int n;
 	srand(time(0));
 	n = rand()  -RAND_MAX /2;
 	if (n>0)
-	{printf("%d is positive\n",n);}
+             printf("%d is positive\n",n);
 	else if (n<0)
-	{printf("%d is negative\n",n);}
+	     printf("%d is negative\n",n);
 	else
-	{printf("d% is zero\n",n);}
+	     printf("%d is zero\n",n);
 	return (0);
 }
