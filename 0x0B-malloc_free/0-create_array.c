39affdeeb3c6d6203df0bf3ef5f780be0b34559e
AndyGrld <ansongandy04@gmail.com>
2023-08-08 18:12:13 +0000

    refactor to betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -12,9 +12,10 @@
 char *create_array(unsigned int size, char c)
 {
 	char *array;
+	unsigned int i;
 
 	array = malloc(sizeof(char) * size);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		array[i] = c;
 	}
