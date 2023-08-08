81134368e2053e6c74a1637fefa6516f24109b93
AndyGrld <ansongandy04@gmail.com>
2023-08-08 17:41:58 +0000

    initial commit

---
+++ 0-create_array.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array: create an array of given size
+ * @size: specifies size of array
+ * @c: first character in array
+ *
+ * Return: nothing
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char array;
+
+	array = malloc(sizeof(char) * size);
+	array[0] = c;
+}
