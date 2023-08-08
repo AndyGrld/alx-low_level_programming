e15aa3c75806142c74013fb74b413d3200a0d0e8
AndyGrld <ansongandy04@gmail.com>
2023-08-08 18:10:35 +0000

    refactor to betty style

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 /**
- * create_array: create an array of given size
+ * create_array - create an array of given size
  * @size: specifies size of array
  * @c: first character in array
  *
@@ -14,7 +14,9 @@ char *create_array(unsigned int size, char c)
 	char *array;
 
 	array = malloc(sizeof(char) * size);
-	array[0] = c;
-	free(array);
+	for (unsigned int i = 0; i < size; i++)
+	{
+		array[i] = c;
+	}
 	return (array);
 }
