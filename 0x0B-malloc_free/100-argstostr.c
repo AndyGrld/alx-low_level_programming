271bcac4355e614094f558187bf106fb65c079f8
AndyGrld <ansongandy04@gmail.com>
2023-08-09 00:53:52 +0000

    add shell script

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,45 +1,44 @@
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 
 /**
- * argstostr - concatenate all arguments of the program
+ * argstostr - concatenates all the arguments of your program.
  * @ac: number of arguments
- * @av: array of arguments
+ * @av: double pointer to arguments
  *
- * Return: pointer to the concatenated string, or NULL if ac == 0 or av == NULL
+ * Return: pointer to new string, or NULL if error
  */
 char *argstostr(int ac, char **av)
 {
-	int total_length = 0;
-	int position = 0;
-	int i, j;
+	int i, j, length = 0, index = 0;
 	char *result;
 
-	if (ac == 0 || av == NULL)
+	if (ac <= 0 || av == NULL)
 		return (NULL);
 
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j]; j++)
-			total_length++;
-		total_length++;
+			length++;
+		length++;
 	}
+	length++;
 
-	result = (char *)malloc(sizeof(char) * total_length);
-
+	result = malloc(length * sizeof(char));
 	if (result == NULL)
-	{
 		return (NULL);
-	}
 
 	for (i = 0; i < ac; i++)
 	{
-		for (j = 0; av[i][j] != '\0'; j++)
+		for (j = 0; av[i][j]; j++)
 		{
-			result[position++] = av[i][j];
+			result[index] = av[i][j];
+			index++;
 		}
-		result[position++] = '\n';
+		result[index] = '\n';
+		index++;
 	}
+	result[index] = '\0';
 
 	return (result);
 }
