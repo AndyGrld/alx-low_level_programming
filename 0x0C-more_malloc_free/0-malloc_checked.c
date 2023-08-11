2745f446ebb40136ca94c972bcd811fe15333645
AndyGrld <ansongandy04@gmail.com>
2023-08-11 00:44:17 +0000

    string concatenate

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - causes process termination if malloc fails
+ * @b: unsigned integer
+ */
+
+void malloc_checked(unsigned int b)
+{
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * malloc_checked - causes process termination if malloc fails
+ * @b: unsigned integer
+ */
+
+void malloc_checked(unsigned int b)
+{
+}
