0c654197f9830483bc9fff696ea59ff802bd3f69
AndyGrld <ansongandy04@gmail.com>
2023-08-11 02:04:36 +0000

    done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,11 +2,10 @@
 #include <stdlib.h>
 
 /**
- * _print - moves a string one place to the left and prints the string
- * @str: string to move
- * @l: size of string
+ * is_digit_string - Checks if a string consists only of digits.
+ * @str: The input string to check.
  *
- * Return: void
+ * Return: 1 if the string consists only of digits, 0 otherwise.
  */
 
 int is_digit_string(const char *str)
@@ -15,13 +14,19 @@ int is_digit_string(const char *str)
 	{
 		if (*str < '0' || *str > '9')
 		{
-			return 0;
+			return (0);
 		}
 		str++;
 	}
-	return 1;
+	return (1);
 }
 
+/**
+ * multiply - Multiplies two positive numbers and prints the result.
+ * @num1: The first positive number as a string.
+ * @num2: The second positive number as a string.
+ *
+ */
 void multiply(const char *num1, const char *num2)
 {
 	if (!is_digit_string(num1) || !is_digit_string(num2))
@@ -31,6 +36,9 @@ void multiply(const char *num1, const char *num2)
 	}
 
 	int len1 = 0, len2 = 0;
+	int i = 0, int j = 0;
+	int start = 0;
+
 	while (num1[len1])
 		len1++;
 	while (num2[len2])
@@ -39,48 +47,46 @@ void multiply(const char *num1, const char *num2)
 	int result_len = len1 + len2;
 	int *result = calloc(result_len, sizeof(int));
 
-	for (int i = len1 - 1; i >= 0; i--)
+	for (i = len1 - 1; i >= 0; i--)
 	{
-		for (int j = len2 - 1; j >= 0; j--)
+		for (j = len2 - 1; j >= 0; j--)
 		{
 			int product = (num1[i] - '0') * (num2[j] - '0');
 			int sum = result[i + j + 1] + product;
+
 			result[i + j + 1] = sum % 10;
 			result[i + j] += sum / 10;
 		}
 	}
-
-	int start = 0;
 	while (start < result_len && result[start] == 0)
-	{
 		start++;
-	}
-
 	if (start == result_len)
-	{
 		printf("0\n");
-	}
 	else
 	{
-		for (int i = start; i < result_len; i++)
-		{
+		for (i = start; i < result_len; i++)
 			printf("%d", result[i]);
-		}
 		printf("\n");
 	}
-
 	free(result);
 }
 
+/**
+ * main - Entry point of the program.
+ * @argc: The number of command-line arguments.
+ * @argv: An array of strings containing the arguments.
+ *
+ * Return: 0 on success, 98 on error.
+ */
 int main(int argc, char *argv[])
 {
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return 98;
+		return (98);
 	}
 
 	multiply(argv[1], argv[2]);
 
-	return 0;
+	return (0);
 }
--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -3,7 +3,7 @@
 
 /**
  * _calloc - memory allocation for array
- * @nmeb: number of array member
+ * @nmemb: number of array member
  * @size: size of elements
  *
  * Return: pointer to array
@@ -12,23 +12,24 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	if (nmemb == 0 || size == 0)
 	{
-		return NULL;
+		return (NULL);
 	}
 
+	unsigned int i;
 	unsigned int total_size = nmemb * size;
 	void *ptr = malloc(total_size);
 
 	if (ptr == NULL)
 	{
-		return NULL;
+		return (NULL);
 	}
 
-	// Initialize allocated memory to zero
 	char *byte_ptr = (char *)ptr;
-	for (unsigned int i = 0; i < total_size; i++)
+
+	for (i = 0; i < total_size; i++)
 	{
 		byte_ptr[i] = 0;
 	}
 
-	return ptr;
+	return (ptr);
 }
