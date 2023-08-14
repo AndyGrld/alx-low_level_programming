8e160b652ce9997b76cd560ba90e3c4123384717
AndyGrld <ansongandy04@gmail.com>
2023-08-14 22:33:18 +0000

    initial commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,14 @@
 #define MAIN_H
 #include <stddef.h>
 
+/**
+ * dog: create a dog structure with features name, age, owner
+ */
 struct dog
 {
 	char *name;
 	float age;
-	char* owner;
+	char *owner;
 };
 
 #endif
